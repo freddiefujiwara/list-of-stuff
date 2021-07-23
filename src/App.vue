b40d45ae77e6fbc1a4ca8b02dce911af19b49591
Fumikazu Fujiwara <github@ze.gs>
2021-07-23 17:38:19 +0900

    create by vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-md-4">
+    <div class="card mb-4 box-shadow">
+      <a style="text-decoration: none;color: #333;" href="{{ item.url }}" target="_blank">
+        <p class="card-text">{{ item.title }}</p>
+        <img class="card-img-top" src="{{ item.image }}" alt="Card image cap">
+      </a>
+      <div class="card-body">
+        <p class="card-text">{{ item.comment }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="javascript:location.href=\'https://freddiefujiwara.com/list-of-stuff/?q=',
+            ncodeURIComponent({{ item.genre }})
+            ">
+              {{ item.genre }}
+            </button>
+          </div>
+          <small class="text-muted">{{ item.price }}円</small>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+const api = "<API>"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      items : null
+    }
+  },
+  async mounted () {
+    this.items =
+      (await axios.get(api)).data
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

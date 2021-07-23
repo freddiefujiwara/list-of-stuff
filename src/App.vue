e3898d47106f788d18c0b716b695342ffad02c05
Fumikazu Fujiwara <github@ze.gs>
2021-07-23 18:39:56 +0900

    add arrayShuffle

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,29 @@
        v-for="(item) in items"
        :key=item.title
        >
-    <div class="card mb-4 box-shadow">
-      <a style="text-decoration: none;color: #333;" v-bind:href="item.url" target="_blank">
-        <p class="card-text">{{ item.title }}</p>
-        <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.title">
-      </a>
-      <div class="card-body">
-        <p class="card-text">{{ item.comment }}</p>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">
-              {{ item.genre }}
-            </button>
-          </div>
-          <small class="text-muted">{{ item.price }}円</small>
-        </div>
-      </div>
-    </div>
+       <div class="card mb-4 box-shadow">
+         <a style="text-decoration: none;color: #333;" v-bind:href="item.url" target="_blank">
+           <p class="card-text">{{ item.title }}</p>
+           <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.title">
+         </a>
+         <div class="card-body">
+           <p class="card-text">{{ item.comment }}</p>
+           <div class="d-flex justify-content-between align-items-center">
+             <div class="btn-group">
+               <button type="button" class="btn btn-sm btn-outline-secondary">
+                 {{ item.genre }}
+               </button>
+             </div>
+             <small class="text-muted">{{ item.price }}円</small>
+           </div>
+         </div>
+       </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import arrayShuffle from 'array-shuffle'
 const api = "<API>"
 export default {
   name: 'App',
@@ -35,7 +36,7 @@ export default {
   },
   async mounted () {
     this.items =
-      (await axios.get(api)).data
+      arrayShuffle((await axios.get(api)).data)
   }
 }
 </script>

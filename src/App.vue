50ffac893bd97d69c4738bb57aceb419883f90e1
Fumikazu Fujiwara | Freddie <github@ze.gs>
2021-07-23 18:06:27 +0900

    Feature/vue nization (#1)
    move to vue.js

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-md-4"
+       contenteditable="true"
+       v-for="(item) in items"
+       :key=item.title
+       >
+    <div class="card mb-4 box-shadow">
+      <a style="text-decoration: none;color: #333;" v-bind:href="item.url" target="_blank">
+        <p class="card-text">{{ item.title }}</p>
+        <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.title">
+      </a>
+      <div class="card-body">
+        <p class="card-text">{{ item.comment }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">
+              {{ item.genre }}
+            </button>
+          </div>
+          <small class="text-muted">{{ item.price }}円</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const api = "<API>"
+export default {
+  name: 'App',
+  data () {
+    return {
+      items : null
+    }
+  },
+  async mounted () {
+    this.items =
+      (await axios.get(api)).data
+  }
+}
+</script>
